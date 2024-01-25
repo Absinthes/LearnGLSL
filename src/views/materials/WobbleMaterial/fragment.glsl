8f36f49fd2e217af8d28593f7928ff5cbdc8ff1e
Absinthe <1198727859@qq.com>
2024-01-25 15:51:56 +0800

    fix: 多余的代码，忘删除了

--- src/views/materials/WobbleMaterial/fragment.glsl
+++ src/views/materials/WobbleMaterial/fragment.glsl
@@ -9,9 +9,7 @@ varying vec2 vUv;
 const float TWO_PI = 6.283185307179586f;
 
 void main() {
-  length(vUv)
-
-    // 根据纹理坐标 vUv 计算出一个二维向量 p
+  // 根据纹理坐标 vUv 计算出一个二维向量 p
   vec2 p = -1.0f + 2.0f * vUv;
   // 计算位置变量 pos，等于时间乘以 2 的派氏常数 π 加上向量 p 的长度乘以尺寸变量 size
   float pos = time * TWO_PI + length(p * size);
